f65e53d8c21f039f962dfe857213442085616752
Raykedd <timlamour39@gmail.com>
2025-10-08 11:40:49 +0200

    build(config): move env files to project root and update backend path

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,12 +3,13 @@ import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { DatabaseModule } from "./modules/database/database.module";
+import { join } from "path";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ".env",
+      envFilePath: [join(__dirname, "..", "..", ".env")],
     }),
     DatabaseModule,
   ],
