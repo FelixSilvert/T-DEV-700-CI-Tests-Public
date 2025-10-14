fcb447502bd58b9185f5a377785583a60310fd02
FelixSilvert <felix.silvert.04@gmail.com>
2025-10-14 12:18:27 +0200

    Feat: Clock update

--- server/src/modules/clocks/entities/clock.entity.ts
+++ server/src/modules/clocks/entities/clock.entity.ts
@@ -1,24 +1,52 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn, CreateDateColumn, UpdateDateColumn } from "typeorm";
 import { User } from "../../users/entities/user.entity";
-import {
-  Column,
-  Entity,
-  JoinColumn,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-} from "typeorm";
+
+/**
+ * Types de pointages possibles
+ * - arrival: Arrivée au travail
+ * - lunchStart: Début de pause déjeuner
+ * - lunchEnd: Fin de pause déjeuner
+ * - departure: Départ du travail
+ */
+export enum ClockType {
+  ARRIVAL = "arrival",
+  LUNCH_START = "lunchStart",
+  LUNCH_END = "lunchEnd",
+  DEPARTURE = "departure",
+}
 
 @Entity("clocks")
 export class Clock {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @Column({ type: "timestamp" })
-  date: Date;
+  @Column({
+    type: "enum",
+    enum: ClockType,
+    comment: "Type de pointage (arrival, lunchStart, lunchEnd, departure)",
+  })
+  type: ClockType;
 
-  @Column({ name: "IDUser", type: "uuid", nullable: true })
+  @Column({
+    type: "timestamp",
+    comment: "Date et heure du pointage",
+  })
+  timestamp: Date;
+
+  @Column({
+    name: "IDUser",
+    type: "uuid",
+    comment: "ID de l'utilisateur ayant effectué le pointage",
+  })
   IDUser: string;
 
-  @ManyToOne(() => User, (user) => user.clocks)
+  @ManyToOne(() => User, (user) => user.clocks, { onDelete: "CASCADE" })
   @JoinColumn({ name: "IDUser" })
   user: User;
+
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
 }
